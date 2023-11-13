c688db970a367c59a615fe1f6e40944abb2043ad
Mitja <mitja.cesar1@student.um.si>
2023-11-13 10:50:50 +0100

    Dodajanje strani Vnos

---
+++ SK10_RIRSU/Pages/Vnos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SK10_RIRSU.Pages
+{
+    public class VnosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
