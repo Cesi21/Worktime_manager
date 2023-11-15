86b3aee07289efde04b774927b96bb18fe3c31b6
Regulus001 <david.brinovec@student.um.si>
2023-11-15 19:18:08 +0100

    Dodan vnos prihodov in odhodov, računanje nadur, filtriranje po uporabnikih

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import app from './firebaseConfig.js';
-import { getFirestore, collection, addDoc} from "firebase/firestore";
-import React, { useState } from 'react';
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import DataTableView from './Podatki.js'; // Predpostavimo, da ste ustvarili to komponento
 
 function App() {
   const [uporabnik, setUporabnik] = useState('');
@@ -32,35 +34,53 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* ... */}
-        <form onSubmit={addDataToFirestore}>
-          <input 
-            type="text" 
-            value={uporabnik} 
-            onChange={(e) => setUporabnik(e.target.value)} 
-            placeholder="Uporabnik" 
-          />
-          <input 
-            type="text" 
-            value={prihod} 
-            onChange={(e) => setPrihod(e.target.value)} 
-            placeholder="Čas prihoda" 
-          />
-          <input 
-            type="text" 
-            value={odhod} 
-            onChange={(e) => setOdhod(e.target.value)} 
-            placeholder="Čas odhoda" 
-          />
-          <button type="submit">Dodaj podatke</button>
-        </form>
-        {/* ... */}
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Domov</Link>
+              </li>
+              <li>
+                <Link to="/data">Prikaži Podatke</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/data" element={<DataTableView />} />
+            <Route path="/" element={
+              <div>
+                <img src={logo} className="App-logo" alt="logo" />
+                <form onSubmit={addDataToFirestore}>
+                  <input 
+                    type="text" 
+                    value={uporabnik} 
+                    onChange={(e) => setUporabnik(e.target.value)} 
+                    placeholder="Uporabnik" 
+                  />
+                  <input 
+                    type="text" 
+                    value={prihod} 
+                    onChange={(e) => setPrihod(e.target.value)} 
+                    placeholder="Čas prihoda" 
+                  />
+                  <input 
+                    type="text" 
+                    value={odhod} 
+                    onChange={(e) => setOdhod(e.target.value)} 
+                    placeholder="Čas odhoda" 
+                  />
+                  <button type="submit">Dodaj podatke</button>
+                </form>
+              </div>
+            }/>
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
 export default App;
-
---
+++ src/Podatki.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react';
+import { getFirestore, collection, query, getDocs } from "firebase/firestore";
+import app from './firebaseConfig';
+
+const DataTableView = () => {
+  const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const db = getFirestore(app);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(db, "test"));
+      const dataList = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+      setData(dataList);
+    };
+
+    fetchData();
+  }, []);
+
+  const calculateOvertime = () => {
+    let totalOvertime = 0;
+    filteredData.forEach(item => {
+    const prihodHours = parseFloat(item.prihod.replace('.', ':').split(':')[0]);
+    const odhodHours = parseFloat(item.odhod.replace('.', ':').split(':')[0]);
+    const delovneUre = odhodHours - prihodHours;
+    const nadure = delovneUre - 8;
+      if (nadure > 0) {
+        totalOvertime = totalOvertime+ nadure;
+      }
+      else if (nadure<0){
+        totalOvertime = totalOvertime+ nadure;
+      }
+    });
+    alert(`Skupno število nadur: ${totalOvertime}`);
+  };
+
+  const filteredData = data.filter(item => item.uporabnik.toLowerCase().includes(searchTerm.toLowerCase()));
+
+  return (
+    <div>
+      <h2>Podatki iz Firestore</h2>
+      <input 
+        type="text" 
+        value={searchTerm} 
+        onChange={(e) => setSearchTerm(e.target.value)} 
+        placeholder="Išči po uporabniku" 
+      />
+      <button onClick={calculateOvertime}>Nadure</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Uporabnik</th>
+            <th>Prihod</th>
+            <th>Odhod</th>
+            <th>Datum</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredData.map((item) => (
+            <tr key={item.id}>
+              <td>{item.uporabnik}</td>
+              <td>{item.prihod}</td>
+              <td>{item.odhod}</td>
+              <td>{item.datum}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default DataTableView;
